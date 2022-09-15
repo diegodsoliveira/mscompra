e3821d48e648872dd644944f0d6c839c3b58c66b
Diego Oliveira <diegodsoliveira@gmail.com>
2022-09-15 19:59:04 -0300

    controle de exceções personalizadas

---
+++ src/main/java/com/diego/mscompra/service/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package com.diego.mscompra.service.exception;
+
+public class EntidadeNaoEncontradaException extends NegocioException {
+    private static final long serialVersionUID = 1L;
+
+    public EntidadeNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+}
